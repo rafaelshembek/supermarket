74e11e5eeeaa67a6e1606216bef9160ebac5e48f
rafaelshembek <rafaelshembek@gmail.com>
2019-07-19 17:12:15 -0300

    index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- iniciar um sessão -->
+<?php session_start(); ?>
+<?php require_once('logica/autoload.php'); ?>
+<?php require_once('interface/_header.php'); ?>
+<?php
+        if(!isset($_SESSION['id_user'])){
+            require_once('interface/menu_top.php');
+        }
+    ?>
+    <?php require_once('interface/barra_top.php'); ?>
+    <?php
+        //  require_once('interface/second_menu_top.php');
+     ?>
+    <?php require_once('interface/body.php'); ?>
+<!-- //////////// BODY //////// CENTRAL -->
+        <!-- <section ng-view></section> -->
+    <?php require_once('interface/tagsbody.php'); ?>
+    <?php 
+        require_once('interface/produtos_vitrini.php'); 
+    ?>
+    <?php 
+        // lojas qualidade em vendas
+        require_once('interface/lojas_quality.php');
+    ?>
+<?php require_once('interface/_footer.php'); ?>
