b6f1175abd02044d4eba6fe5f5cc1f693990c568
rafaelshembek <rafaelshembek@gmail.com>
2019-08-02 13:48:37 -0300

    alterações no arquivo de deletar os produtos do carrinho

--- logica/delete_produto_carrinho.php
+++ logica/delete_produto_carrinho.php
@@ -8,7 +8,7 @@ if(isset($_GET['del']) && isset($_GET['refLoja'])){
     $params = array(
         ':id_delete' => $id_delete
     );
-    $el = $delete->exe_query("DELETE FROM carrinho WHERE carrinho.id_carrinho = :id_delete", $params);
+    $el = $delete->exe_query("DELETE FROM carrinho WHERE id_carrinho = :id_delete", $params);
     header("Location:../../../interface/carrinho/".$_GET['refLoja']);
 }else{
     echo 'erro';
