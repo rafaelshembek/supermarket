a471c9611cc8fe802bc51d698f851f5f895db0a8
rafaelshembek <rafaelshembek@gmail.com>
2019-08-04 11:25:58 -0300

    alterações no index

--- index.php
+++ index.php
@@ -2,21 +2,11 @@
 <?php session_start(); ?>
 <?php require_once('logica/autoload.php'); ?>
 <?php require_once('interface/_header.php'); ?>
-<?php
-        if(!isset($_SESSION['id_user'])){
-            require_once('interface/menu_top.php');
-        }
-    ?>
-    <?php require_once('interface/barra_top.php'); ?>
-    <?php require_once('interface/body.php'); ?>
+<?php require_once('interface/menu_top.php'); ?>
+<?php require_once('interface/body.php'); ?>
         <!-- <section ng-view></section> -->
-    <?php require_once('interface/barra_information.php'); ?>
-    <?php require_once('interface/tagsbody.php'); ?>
-    <?php 
-        require_once('interface/produtos_vitrini.php'); 
-    ?>
-    <?php 
-        // lojas qualidade em vendas
-        require_once('interface/product_more_bay.php');
-    ?>
+<?php require_once('interface/barra_information.php'); ?>
+<?php require_once('interface/tagsbody.php'); ?>
+<?php  require_once('interface/produtos_vitrini.php'); ?>
+<?php require_once('interface/product_more_bay.php'); ?>
 <?php require_once('interface/_footer.php'); ?>
